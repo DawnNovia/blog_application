963b7248d070b1bbe37f813ebf81f73629a89c47
DawnNovia <dawn.edwards@hotmail.com>
2022-08-10 00:52:28 -0400

    comment edits, javascript packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,15 @@
 
 console.log('Hello World from Webpacker')
 import "actiontext"
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+import "@hotwired/turbo-rails"
+import "controllers"
+import "trix"
+import "@rails/actiontext"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'dawn@example.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'dawn@example.com', name: "Dawn", password: 'password', password_confirmation: 'password')
+User.create(email: 'dawn1@example.com', name: "Dawn1", password: 'password', password_confirmation: 'password')
 
 10.times do |x|
     Post.create(title: "Title #{x+1}", body: "Body #{x+1} Words go here IDK", user_id: User.first.id)
